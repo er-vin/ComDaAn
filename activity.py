551a3585095797ebf4dae50753b3a8d492f0ac46
Kevin Ottens <ervin@ipsquad.net>
2018-03-05 20:22:41 +0100

    Add support for a richer color palette
    The old legacy blue blobs become the blue4 palette, for the new palette
    we simply use the Magma256 palette from bokeh but reversed. This new
    palette is also the new default.

--- activity.py
+++ activity.py
@@ -23,6 +23,7 @@ from gitparsing import GitParser
 from bokeh.plotting import figure, show
 from bokeh.models import HoverTool
 from bokeh.models.sources import ColumnDataSource
+from bokeh.palettes import Magma256
 from bokeh.io import output_notebook, output_file
 
 
@@ -30,6 +31,7 @@ if __name__ == "__main__":
     # Parse the args before all else
     arg_parser = argparse.ArgumentParser(description = "A tool for visualizing, week by week, who contributes code and how frequently")
     arg_parser.add_argument("paths", metavar = "path", nargs = "+", help = "Path of a git repository to process or of a directory containing git repositories")
+    arg_parser.add_argument("--palette", choices = ["blue4", "magma256"], default = "magma", help = "Choose a palette (default is magma256)")
     arg_parser.add_argument("-f", "--start", help = "Start date")
     arg_parser.add_argument("-u", "--end", help = "End date")
     arg_parser.add_argument("-t", "--title", help = "Title")
@@ -61,10 +63,14 @@ if __name__ == "__main__":
 
     weekly_activity = daily_activity.groupby('author_name').resample("W", level = 1).sum()
     weekly_activity = weekly_activity.loc[lambda x: x['count'] > 0]
-    weekly_activity['alpha'] = weekly_activity['count'].apply(lambda x: min(1.0, x * 0.20))
     weekly_activity = weekly_activity.reset_index(level = ['author_name', 'date'])
     weekly_activity['date'] = weekly_activity['date'].apply(lambda x: x - timedelta(days = 3))
     weekly_activity['week_name'] = weekly_activity['date'].apply(lambda x: "%s-%s" % x.isocalendar()[:2])
+
+    palette = list(reversed(Magma256))
+    if args.palette == "blue4":
+        palette = ["#EAF5F9", "#D6EBF2", "#C1E2EC", "#ADD8E6"]
+    weekly_activity['color'] = weekly_activity['count'].apply(lambda x: palette[min(len(palette) - 1, x)])
 
     output_file(output_filename)
     p = figure(x_axis_type = "datetime", y_range = authors,
@@ -77,8 +83,8 @@ if __name__ == "__main__":
     p.rect("date", "author_name",
            source = ColumnDataSource(weekly_activity),
            color = "lightblue",
-           fill_alpha = "alpha",
-           line_alpha = "alpha",
+           fill_color = "color",
+           line_color = "color",
            width = 1000 * 60 * 60 * 24 * 7,
            height = 1)
     show(p)
