77a63bb1723a8e33c1590f9e98a2302ee312ac99
Christelle Zouein <christelle.zouein@epita.fr>
2019-10-02 16:34:39 +0200

    Activity analysis for mailing lists
    Activity analysing for mailing lists heavily inspired for its git
    counterpart.

---
+++ mail_activity.py
@@ -0,0 +1,89 @@
+#
+# Copyright 2019 Christelle Zouein <christellezouein@hotmail.com>
+#
+# The authors license this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+from argparse import ArgumentParser
+from datetime import datetime
+
+from mailparsing import MailParser
+
+from bokeh.plotting import figure, show
+from bokeh.models import HoverTool, LinearColorMapper
+from bokeh.models.sources import ColumnDataSource
+from bokeh.palettes import Magma256
+from bokeh.io import output_file
+
+if __name__ == "__main__":
+    # Parse the args before all else
+    arg_parser = ArgumentParser(
+        description="A tool for visualizing, week by week, who sends emails", parents=[MailParser.get_argument_parser()]
+    )
+    arg_parser.add_argument(
+        "--palette", choices=["blue4", "magma256"], default="magma", help="Choose a palette (default is magma256)"
+    )
+    arg_parser.add_argument("-t", "--title", help="Title")
+    arg_parser.add_argument("-o", "--output", help="Output file (default is 'result.html')")
+
+    args = arg_parser.parse_args()
+
+    start_date = args.start
+    end_date = args.end
+
+    output_filename = args.output or "result.html"
+
+    parser = MailParser()
+    parser.add_archives(args.paths)
+    emails = parser.get_emails(start_date, end_date)
+    emails["date"] = emails["date"].apply(lambda x: datetime(year=x.year, month=x.month, day=x.day))
+
+    start_dates = emails.groupby("sender_name")["sender_name", "date"].min()
+    start_dates.index.name = "name_index"
+    senders = start_dates.sort_values(["date", "sender_name"], ascending=False).loc[:, "sender_name"].tolist()
+
+    daily_activity = emails.loc[:, ["sender_name", "date", "message_id"]].groupby(["sender_name", "date"]).count()
+    daily_activity.columns = ["count"]
+
+    weekly_activity = daily_activity.groupby("sender_name").resample("W", level=1).sum()
+    weekly_activity = weekly_activity.loc[lambda x: x["count"] > 0]
+    weekly_activity = weekly_activity.reset_index(level=["sender_name", "date"])
+    weekly_activity["week_name"] = weekly_activity["date"].apply(lambda x: "%s-%s" % x.isocalendar()[:2])
+
+    palette = list(reversed(Magma256))
+    color_mapper = LinearColorMapper(
+        palette=palette, low=weekly_activity["count"].min(), high=weekly_activity["count"].max()
+    )
+    if args.palette == "blue4":
+        palette = ["#EAF5F9", "#D6EBF2", "#C1E2EC", "#ADD8E6"]
+        color_mapper = LinearColorMapper(palette=palette, low=0, high=4)
+
+    output_file(output_filename)
+    p = figure(
+        x_axis_type="datetime",
+        y_range=senders,
+        sizing_mode="stretch_both",
+        active_scroll="wheel_zoom",
+        title=args.title,
+    )
+    p.add_tools(HoverTool(tooltips=[("Sender", "@sender_name"), ("Week", "@week_name"), ("Count", "@count")]))
+    p.rect(
+        "date",
+        "sender_name",
+        source=ColumnDataSource(weekly_activity),
+        fill_color={"field": "count", "transform": color_mapper},
+        line_color={"field": "count", "transform": color_mapper},
+        width=1000 * 60 * 60 * 24 * 7,
+        height=1,
+    )
+    show(p)
