dcfc984d91df6d1eb4edfb07a21c687a7e8d00c1
Christelle Zouein <christelle.zouein@epita.fr>
2019-11-28 17:19:21 +0100

    Teamsize function added for use with all data sources
    Commenter and/or reporter choice added for teamsize analysis

--- comdaan.py
+++ comdaan.py
@@ -20,12 +20,14 @@ from collections import namedtuple
 from datetime import datetime, timedelta
 from itertools import combinations
 from functools import reduce
+from statsmodels.nonparametric.smoothers_lowess import lowess
 
 from gitparsing import _GitParser
 from mailparsing import _MailParser
 from issuesparsing import _IssuesParser
 
 Activity = namedtuple("Activity", ["dataframe", "authors"])
+TeamSize = namedtuple("TeamSize", ["dataframe"])
 Network = namedtuple("Network", ["dataframe"])
 
 
@@ -92,7 +94,55 @@ def activity(dataframe, id_col_name, author_col_name, date_col_name, actor="repo
     return Activity(weekly_activity, authors)
 
 
-# If the source and target columns are the same, only the source needs to be given.
+def teamsize(dataframe, id_col_name, author_col_name, date_col_name, actor="reporter", frac=None):
+    author_team_size = pd.DataFrame()
+    comm_team_size = pd.DataFrame()
+
+    if "reporter" in actor:
+        dataframe[date_col_name] = dataframe[date_col_name].apply(lambda x: x.date())
+        dataframe[date_col_name] = pd.DatetimeIndex(dataframe[date_col_name]).to_period("W").to_timestamp()
+        dataframe[date_col_name] = dataframe[date_col_name].apply(lambda x: x - timedelta(days=3))
+
+        dataframe_by_date = dataframe.groupby(date_col_name)
+
+        author_team_size["entry_count"] = dataframe_by_date[id_col_name].count()
+        author_team_size["author_count"] = dataframe_by_date[author_col_name].nunique()
+
+    if "commenter" in actor:
+        dataframe = dataframe.explode("discussion").rename(columns={"discussion": "comment"})
+        dataframe["comment_author"] = dataframe["comment"].apply(lambda comment: comment[author_col_name])
+        dataframe["comment_created_at"] = dataframe["comment"].apply(lambda comment: comment[date_col_name])
+
+        comments = dataframe.loc[:, ["comment_author", "comment_created_at"]].rename(
+            columns={"comment_author": author_col_name, "comment_created_at": date_col_name}
+        )
+        comments[date_col_name] = comments[date_col_name].apply(lambda x: x.date())
+        comments[date_col_name] = pd.DatetimeIndex(comments[date_col_name]).to_period("W").to_timestamp()
+        comments[date_col_name] = comments[date_col_name].apply(lambda x: x - timedelta(days=3))
+        comments[id_col_name] = comments.index
+
+        comments_by_date = comments.groupby(date_col_name)
+
+        comm_team_size["entry_count"] = comments_by_date[id_col_name].count()
+        comm_team_size["author_count"] = comments_by_date[author_col_name].nunique()
+
+    team_size = pd.concat([author_team_size, comm_team_size], sort=False)
+    team_size = team_size.groupby(date_col_name).sum()
+    team_size = team_size.sort_values(by=date_col_name)
+    team_size.reset_index(inplace=True)
+
+    y_a = team_size["entry_count"].values
+    y_ac = team_size["author_count"].values
+    x = team_size[date_col_name].apply(lambda date: date.timestamp()).values
+
+    frac = float(frac) if frac is not None else 10 * len(x) ** (-0.75)
+
+    team_size["entry_count_lowess"] = lowess(y_a, x, is_sorted=True, frac=frac if frac < 1 else 0.8, it=0)[:, 1]
+    team_size["author_count_lowess"] = lowess(y_ac, x, is_sorted=True, frac=frac if frac < 1 else 0.8, it=0)[:, 1]
+
+    return TeamSize(team_size)
+
+
 def network(dataframe, author_col_name, target_col_name, source_col_name=None):
     # In the case of issues and "discussion" needs processing
     if isinstance(dataframe[target_col_name].iloc[0], list) and isinstance(dataframe[target_col_name].iloc[0][0], dict):
@@ -106,7 +156,7 @@ def network(dataframe, author_col_name, target_col_name, source_col_name=None):
 
         for i in range(len(authors)):
             edges.extend([(authors[i], commenter) for commenter in commenter_threads[i]])
-        
+
         edge_list = pd.DataFrame(edges, columns=["source", "target"])
         edge_list = edge_list.groupby(["source", "target"]).size().reset_index(name="weight")
 
